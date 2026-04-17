f65e86d215a375731036a9629de316314daccce4
DeepSeek <noreply@deepseek.com>
2026-04-17 13:04:35 +0800

    Create cli.h

---
+++ include/cli.h
@@ -0,0 +1,37 @@
+#ifndef ATP_CLI_H
+#define ATP_CLI_H
+
+#include <getopt.h>
+#include <stdbool.h>
+
+typedef enum {
+    CMD_NONE = 0,
+    CMD_START,
+    CMD_STOP,
+    CMD_RESTART,
+    CMD_STATUS,
+    CMD_UPDATE_GEOIP,
+    CMD_RELOAD,
+    CMD_VERSION,
+    CMD_HELP
+} atp_command_t;
+
+typedef struct {
+    atp_command_t command;
+    char config_dir[PATH_MAX];
+    int dry_run;
+    int verbose;
+    int quiet;
+    int foreground;
+    int use_syslog;
+    char log_file[PATH_MAX];
+    log_level_t log_level;
+} atp_options_t;
+
+void print_usage(const char *progname);
+void print_version(void);
+void print_help(const char *progname);
+int parse_arguments(int argc, char *argv[], atp_options_t *opts);
+const char* command_to_string(atp_command_t cmd);
+
+#endif
