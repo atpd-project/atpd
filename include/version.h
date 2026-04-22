6606384c4c10c390d12fad74920d18a7480f4908
DeepSeek <noreply@deepseek.com>
2026-04-23 06:54:48 +0800

    chore(version): regenerate version.h with clean v0 scheme

--- include/version.h
+++ include/version.h
@@ -1,14 +1,15 @@
+/*
+ * ATP - Advanced Transparent Proxy
+ * Copyright (C) 2024-2026 ATP Project
+ *
+ * Auto-generated version header - DO NOT EDIT
+ */
+
 #ifndef ATP_VERSION_H
 #define ATP_VERSION_H
 
-#define ATP_VERSION_MAJOR     0
-#define ATP_VERSION_MINOR     0
-#define ATP_VERSION_PATCH     1
-#define ATP_VERSION_STRING    "0.0.1-90411f9"
+#define ATP_VERSION_STRING  "v0.364e1ce-dirty"
+#define ATP_VERSION         "v0.364e1ce-dirty"
+#define ATP_COMMIT          "364e1ce"
 
-#endif
-const char* atp_get_version(void);
-int atp_get_version_major(void);
-int atp_get_version_minor(void);
-int atp_get_version_patch(void);
-const char* atp_get_version(void);
+#endif /* ATP_VERSION_H */
