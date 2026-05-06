313dc8908eb1dbb62779b4faa259c186966e3134
debiansid <9624961+debiansid@users.noreply.github.com>
2026-05-06 22:20:32 +0800

    fix(ipv6_manager): add dry_run guards, fix fscanf check, pass cfg through all functions

--- include/ipv6_manager.h
+++ include/ipv6_manager.h
@@ -4,9 +4,9 @@
 #include "atp.h"
 
 typedef enum {
-    IPV6_MODE_DEFAULT = 0,      /* System default */
-    IPV6_MODE_PROXY = 1,        /* IPv6 proxy enabled */
-    IPV6_MODE_DISABLED = -1     /* IPv6 completely disabled */
+    IPV6_MODE_DEFAULT = 0,
+    IPV6_MODE_PROXY = 1,
+    IPV6_MODE_DISABLED = -1
 } ipv6_mode_t;
 
 typedef struct {
@@ -19,10 +19,10 @@ typedef struct {
 
 int ipv6_manager_init(atp_config_t *cfg);
 int ipv6_manager_set_mode(atp_config_t *cfg, int mode);
-int ipv6_manager_backup(ipv6_backup_t *backup);
-int ipv6_manager_restore(ipv6_backup_t *backup);
-int ipv6_manager_disable_all(void);
-int ipv6_manager_enable_all(void);
+int ipv6_manager_backup(atp_config_t *cfg, ipv6_backup_t *backup);
+int ipv6_manager_restore(atp_config_t *cfg, ipv6_backup_t *backup);
+int ipv6_manager_disable_all(atp_config_t *cfg);
+int ipv6_manager_enable_all(atp_config_t *cfg);
 int ipv6_manager_is_disabled(void);
 
 #endif
