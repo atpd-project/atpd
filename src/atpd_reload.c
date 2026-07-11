74b06db3b1e255892a8f6b63894efb9216a4c474
debiansid <9624961+debiansid@users.noreply.github.com>
2026-07-11 21:02:46 +0800

    fix: remove ebpf_reload call from atpd_reload.c

--- src/atpd_reload.c
+++ src/atpd_reload.c
@@ -8,10 +8,7 @@
 #include "atpd_global.h"
 #include "logger.h"
 #include "config.h"
-#include "boxbpf.h"
 #include "app_filter.h"
-#include "service.h"
-#include "api.h"
 
 #include <string.h>
 #include <time.h>
@@ -26,17 +23,5 @@ int atpd_reload_config(void) {
     }
     
     LOG_INFO("Configuration reloaded successfully");
-    return 0;
-}
-
-int atpd_apply_config(void) {
-    if (g_config.app_proxy_enable) {
-        app_filter_reload(&g_config);
-    }
-    
-    if (g_config.ebpf_enabled && g_config.bypass_cn_ip) {
-        ebpf_reload(&g_config);
-    }
-    
     return 0;
 }
