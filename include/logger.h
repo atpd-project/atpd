e4f238fcd865c7366249bb658b65f538874ab7a8
debiansid <9624961+debiansid@users.noreply.github.com>
2026-07-11 19:36:03 +0800

    fix: remove file/line info from log output

--- include/logger.h
+++ include/logger.h
@@ -40,31 +40,31 @@ static inline void atpd_log_init(void) {
 #define LOG_DEBUG(fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "[DEBUG] " fmt, ##__VA_ARGS__); \
-    log_write(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); \
+    log_write(LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_INFO(fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(ANDROID_LOG_INFO, LOG_TAG, "[INFO] " fmt, ##__VA_ARGS__); \
-    log_write(LOG_LEVEL_INFO, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); \
+    log_write(LOG_LEVEL_INFO, fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_WARN(fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(ANDROID_LOG_WARN, LOG_TAG, "[WARN] " fmt, ##__VA_ARGS__); \
-    log_write(LOG_LEVEL_WARN, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); \
+    log_write(LOG_LEVEL_WARN, fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_ERROR(fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(ANDROID_LOG_ERROR, LOG_TAG, "[ERROR] " fmt, ##__VA_ARGS__); \
-    log_write(LOG_LEVEL_ERROR, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); \
+    log_write(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_FATAL(fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(ANDROID_LOG_FATAL, LOG_TAG, "[FATAL] " fmt, ##__VA_ARGS__); \
-    log_write(LOG_LEVEL_FATAL, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); \
+    log_write(LOG_LEVEL_FATAL, fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_DEBUG_LAZY(fmt, ...) LOG_DEBUG(fmt, ##__VA_ARGS__)
@@ -74,31 +74,31 @@ static inline void atpd_log_init(void) {
 #define LOG_SERVICE(level, fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(level, LOG_TAG, "[SERVICE] " fmt, ##__VA_ARGS__); \
-    log_write(level, __FILE__, __LINE__, __FUNCTION__, "[SERVICE] " fmt, ##__VA_ARGS__); \
+    log_write(level, "[SERVICE] " fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_API(level, fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(level, LOG_TAG, "[API] " fmt, ##__VA_ARGS__); \
-    log_write(level, __FILE__, __LINE__, __FUNCTION__, "[API] " fmt, ##__VA_ARGS__); \
+    log_write(level, "[API] " fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_ROUTE(level, fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(level, LOG_TAG, "[ROUTE] " fmt, ##__VA_ARGS__); \
-    log_write(level, __FILE__, __LINE__, __FUNCTION__, "[ROUTE] " fmt, ##__VA_ARGS__); \
+    log_write(level, "[ROUTE] " fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_NETLINK(level, fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(level, LOG_TAG, "[NETLINK] " fmt, ##__VA_ARGS__); \
-    log_write(level, __FILE__, __LINE__, __FUNCTION__, "[NETLINK] " fmt, ##__VA_ARGS__); \
+    log_write(level, "[NETLINK] " fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_REACTOR(level, fmt, ...) do { \
     atpd_log_init(); \
     if (atpd_log_print) atpd_log_print(level, LOG_TAG, "[REACTOR] " fmt, ##__VA_ARGS__); \
-    log_write(level, __FILE__, __LINE__, __FUNCTION__, "[REACTOR] " fmt, ##__VA_ARGS__); \
+    log_write(level, "[REACTOR] " fmt, ##__VA_ARGS__); \
 } while(0)
 
 #define LOG_EXEC(cmd) LOG_DEBUG("[EXEC] %s", cmd)
@@ -164,40 +164,40 @@ void log_write(log_level_t level, const char *file, int line, const char *func,
 /* ========== Standard Log Macros ========== */
 
 #define LOG_DEBUG(fmt, ...) \
-    log_write(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+    log_write(LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
 
 #define LOG_INFO(fmt, ...) \
-    log_write(LOG_LEVEL_INFO, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+    log_write(LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
 
 #define LOG_WARN(fmt, ...) \
-    log_write(LOG_LEVEL_WARN, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+    log_write(LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
 
 #define LOG_ERROR(fmt, ...) \
-    log_write(LOG_LEVEL_ERROR, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+    log_write(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
 
 #define LOG_FATAL(fmt, ...) \
-    log_write(LOG_LEVEL_FATAL, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+    log_write(LOG_LEVEL_FATAL, fmt, ##__VA_ARGS__)
 
 /* ========== P1: Lazy Evaluation Macros (Zero-cost when disabled) ========== */
 
 #define LOG_DEBUG_LAZY(fmt, ...) do { \
     extern log_config_t g_log_config; \
     if (g_log_config.min_level <= LOG_LEVEL_DEBUG) { \
-        log_write(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); \
+        log_write(LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__); \
     } \
 } while(0)
 
 #define LOG_INFO_LAZY(fmt, ...) do { \
     extern log_config_t g_log_config; \
     if (g_log_config.min_level <= LOG_LEVEL_INFO) { \
-        log_write(LOG_LEVEL_INFO, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); \
+        log_write(LOG_LEVEL_INFO, fmt, ##__VA_ARGS__); \
     } \
 } while(0)
 
 #define LOG_WARN_LAZY(fmt, ...) do { \
     extern log_config_t g_log_config; \
     if (g_log_config.min_level <= LOG_LEVEL_WARN) { \
-        log_write(LOG_LEVEL_WARN, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); \
+        log_write(LOG_LEVEL_WARN, fmt, ##__VA_ARGS__); \
     } \
 } while(0)
 
