4f6166e3a0f118ae21c647cd380738deb16d73c1
debiansid <9624961+debiansid@users.noreply.github.com>
2026-07-12 06:49:07 +0800

    refactor: modularize main.c and enhance service management
    - Split main.c into modular components (atpd_init, atpd_reload, atpd_shutdown)
    - Extended atpd_context_t with runtime state, components, stats, errors
    - Enhanced service management with circuit breaker, health check, configurable timeouts
    - Added service_args and service_env support
    - Added service log rotation with size limit
    - Added service_is_healthy() API
    - Added all missing fields to atp_config_t

--- include/atp.h
+++ include/atp.h
@@ -110,10 +110,22 @@ typedef struct {
     /* Core */
     int foreground;
     int verbose;
+    int no_color;
+    int ui_emoji_enabled;
+    int performance_mode;
+    int dry_run;
+    int skip_check_feature;
+    int force_mark_bypass;
+    int log_timestamp;
+    int restart_delay;
+    int proxy_tcp;
+    int proxy_udp;
+    int block_quic;
     char data_dir[PATH_MAX];
     char core_user[64];
     char core_group[64];
     char pid_file[PATH_MAX];
+    char routing_mark[32];
 
     /* Network */
     int use_tproxy;
@@ -140,30 +152,40 @@ typedef struct {
     int proxy_hotspot;
     int proxy_usb;
 
-    /* Filters */
-    int app_proxy_enable;
-    char app_proxy_mode[32];
+    /* Other interfaces */
     char other_proxy[4096];
     char other_bypass[4096];
+
+    /* IP Lists */
     char bypass_ipv4_list[4096];
     char bypass_ipv6_list[4096];
     char proxy_ipv4_list[4096];
     char proxy_ipv6_list[4096];
-    int mac_filter_enable;
-    char mac_proxy_mode[32];
+
+    /* CNIP */
     int bypass_cn_ip;
+    int cnip_mode;
     char cn_ip_url[256];
     char cn_ip_file[64];
     char cn_ipv6_url[256];
     char cn_ipv6_file[64];
-    int cnip_mode;
     char cnip_force_proxy_apps[4096];
-    char bypass_apps_list[4096];
-    char proxy_apps_list[4096];
 
-    /* Performance */
-    int performance_mode;
-    int dry_run;
+    /* App Filter */
+    int app_proxy_enable;
+    char app_proxy_mode[32];
+    char proxy_apps_list[4096];
+    char bypass_apps_list[4096];
+
+    /* MAC Filter */
+    int mac_filter_enable;
+    char mac_proxy_mode[32];
+    char proxy_macs_list[4096];
+    char bypass_macs_list[4096];
+
+    /* Clash */
+    char user_clash_mode[32];
+    char clash_secret[128];
 
     /* API */
     int api_port;
@@ -172,17 +194,14 @@ typedef struct {
     /* eBPF */
     int ebpf_enabled;
     int ebpf_ready;
+    int ebpf_load_retry;
+    int ebpf_load_delay;
+    char ebpf_bin_path[PATH_MAX];
     char ebpf_config_path[PATH_MAX];
     char ebpf_pin_dir[PATH_MAX];
     char ebpf_state_dir[PATH_MAX];
-    int ebpf_load_retry;
-    int ebpf_load_delay;
 
-    /* UI */
-    int ui_emoji_enabled;
-    int no_color;
-
-    /* Service configuration */
+    /* Service */
     int service_start_timeout_sec;
     int service_stop_timeout_sec;
     int service_grace_period_sec;
@@ -192,6 +211,9 @@ typedef struct {
     int service_health_check_interval_ms;
     char service_args[512];
     char service_env[512];
+
+    /* Mutex */
+    pthread_mutex_t config_mutex;
 
 } atp_config_t;
 
