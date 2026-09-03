7454dae080acdc3e421a259763d515fed3ebb474
debiansid <9624961+debiansid@users.noreply.github.com>
2026-09-03 12:10:03 +0800

    fix(tests): avoid assert side-effects in test_utils_proc_stat

--- tests/test_utils_proc_stat.c
+++ tests/test_utils_proc_stat.c
@@ -1,6 +1,8 @@
 #include "utils.h"
 
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <string.h>
 #include <sys/prctl.h>
@@ -17,14 +19,13 @@ int main(int argc, char *argv[]) {
     }
 
     unsigned long long self_start = 0;
-    assert(get_process_starttime(getpid(), &self_start) == 0);
-    assert(self_start != 0);
+    if (get_process_starttime(getpid(), &self_start) != 0 || self_start == 0) abort();
 
     int sync_pipe[2];
-    assert(pipe(sync_pipe) == 0);
+    if (pipe(sync_pipe) != 0) abort();
 
     pid_t child = fork();
-    assert(child >= 0);
+    if (child < 0) abort();
     if (child == 0) {
         close(sync_pipe[1]);
         if (dup2(sync_pipe[0], STDIN_FILENO) < 0) _exit(126);
@@ -39,18 +40,14 @@ int main(int argc, char *argv[]) {
         (void)get_process_starttime(child, &child_start);
         if (child_start == 0) usleep(1000);
     }
-    assert(child_start != 0);
+    if (child_start == 0) abort();
 
     unsigned long long child_start_again = 0;
-    assert(get_process_starttime(child, &child_start_again) == 0);
-    assert(child_start_again == child_start);
+    if (get_process_starttime(child, &child_start_again) != 0 || child_start_again != child_start) abort();
 
     close(sync_pipe[1]);
     int status = 0;
-    assert(waitpid(child, &status, 0) == child);
-    assert(WIFEXITED(status) && WEXITSTATUS(status) == 0);
-    (void)self_start;
-    (void)child_start_again;
-    (void)status;
+    pid_t w = waitpid(child, &status, 0);
+    if (w != child || !WIFEXITED(status) || WEXITSTATUS(status) != 0) abort();
     return 0;
 }
